ff460c1d00b4ec8205f882e74b6131cd6b821078
dawit <david8060g@gmail.com>
2021-11-07 00:24:22 +0200

    satr

--- week5/day 5 mini project/ajax.js
+++ week5/day 5 mini project/ajax.js
@@ -1,21 +1,23 @@
 async function fetchStar() {
 try {
-
   let rand = Math.round(Math.random() * 82);
-  const response = await fetch(`https://swapi.dev/api/people/${rand}/`)
-  const data = await response.json();
+  let response = await fetch(`https://swapi.dev/api/people/${rand}/`)
+  let data = await response.json();
      console.log(data);
      document.getElementById("name").innerHTML = data.name;
      document.getElementById("height").innerHTML = data.height;
      document.getElementById("gender").innerHTML = data.gender;
-     document.getElementById("byear").innerHTML = data.birth_year;
+     document.getElementById("birth_year").innerHTML = data.birth_year;
+     document.getElementById("homeworld").innerHTML = data.homeworld;
    } catch (error) {
-       console.log(error)
+       console.log(error, "Oh No! That person isnt available");
    }
   }
+ 
   
-
+  
 fetchStar()
+document.getElementsByClassName("i").innerHTML;
 
 function Star(){
   let button = document.getElementById("btnsearch");
